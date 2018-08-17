5a5493ca42994c15e415938f52695b2a7df0a9a3
HelmutSteiner <helmut.steinersti2@gmail.com>
2018-08-17 16:02:39 +0200

    Initial commit.

---
+++ src/main/java/com/enterprise/app/ranchhand/RanchHandApplication.java
@@ -0,0 +1,12 @@
+package com.enterprise.app.ranchhand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RanchHandApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RanchHandApplication.class, args);
+  }
+}
